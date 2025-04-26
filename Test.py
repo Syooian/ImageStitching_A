6307e676bc2b528f975485a2f61176b402b17b47
Syooian <msn0126@hotmail.com>
2025-04-26 22:57:17 +0800

    接兩個WebCam畫面

--- Test.py
+++ Test.py
@@ -35,28 +35,39 @@ def main():
 
 
 
-    cap = cv2.VideoCapture(0)
+    cap0 = cv2.VideoCapture(0)
+    cap1 = cv2.VideoCapture(1)
 
-    if not cap.isOpened():
-        print("無法開啟攝影機")
+    if not cap0.isOpened():
+        print("無法開啟攝影機0")
+        return
+    if not cap1.isOpened():
+        print("無法開啟攝影機1")
         return
 
     while True:
         # 讀取攝影機畫面
-        ret, frame = cap.read()
-        if not ret:
-            print("無法讀取畫面")
+        ret0, frame0 = cap0.read()
+        if not ret0:
+            print("無法讀取畫面0")
             break
-
         # 顯示畫面
-        cv2.imshow('WebCam', frame)
+        cv2.imshow('WebCam0', frame0)
+
+        ret1, frame1 = cap1.read()
+        if not ret1:
+            print("無法讀取畫面1")
+            break
+        # 顯示畫面
+        cv2.imshow('WebCam1', frame1)
 
         # 按下 'q' 鍵退出
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # 釋放資源
-    cap.release()
+    cap0.release()
+    cap1.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
